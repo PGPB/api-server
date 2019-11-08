8c013f84866ded48eae53059ceef9b72f2f097b2
Satyuel <satyuel+git@localhost>
2019-11-08 17:27:31 +0300

    Add some handlers

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,14 @@
-# from aiohttp import web
+from aiohttp import web
 from .controller import *
+from .auth import *
 
 
 def setup_routes(app):
     routes = [
         web.route('GET', '/test', test_controller),
-
+        web.route('GET', '/auth', auth),
+        web.route('GET', '/login', login_handler),
+        web.route('GET', '/signup', signup_handler),
+        web.route('GET', '/getuser', getuser_handler),
     ]
     app.add_routes(routes)
