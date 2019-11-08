c3064b74af944a086f3c0755dbec8cdb2e236627
Satyuel <satyuel+git@localhost>
2019-11-09 00:45:12 +0300

    Finish get_user function

--- api/model.py
+++ api/model.py
@@ -1,13 +1,12 @@
-import asyncio
-import pymongo.errors
-from bson.json_util import dumps
 from bson.objectid import ObjectId
+import json
 import mongoengine
 
 from .db import *
 
 
-async def create_user(data):
+async def create_user(data: dict) -> str:
+    """ Insert new document to User collection and return document _id """
     try:
         user = User(
             login=data['login'],
@@ -16,7 +15,7 @@ async def create_user(data):
             middle_name=data['middle_name'],
             last_name=data['last_name']
         ).save()
-        return str(user.pk)
+        return str(user.id)
     except mongoengine.errors.NotUniqueError:
         raise Exception('Login has occupied already')
     except KeyError as e:
@@ -24,5 +23,8 @@ async def create_user(data):
 
 
 async def get_user_by_credentials(login, password):
-    user = User.objects(login=login, password=password).first()
-    ...
+    """ Return user id and his status or None if user don't exist"""
+    user = User.objects(login=login, password=password).only('status').first()
+    if user:
+        return json.loads((user.to_json()))
+    return None
