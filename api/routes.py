e9a2dd36720d803ad139ab1ec908f315a46eca2b
Satyuel <satyuel+git@localhost>
2019-11-08 19:14:39 +0300

    Some changes

--- api/routes.py
+++ api/routes.py
@@ -5,10 +5,9 @@ from .auth import *
 
 def setup_routes(app):
     routes = [
-        web.route('GET', '/test', test_controller),
         web.route('GET', '/auth', auth),
         web.route('GET', '/login', login_handler),
-        web.route('GET', '/signup', signup_handler),
+        web.route('POST', '/signup', signup_handler),
         web.route('GET', '/getuser', getuser_handler),
     ]
     app.add_routes(routes)
