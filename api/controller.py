195e4b48eca7f8f3ee075487eaccbe022a1a7c67
Satyuel <satyuel+git@localhost>
2019-11-08 19:11:26 +0300

    Delete unused raws

--- api/controller.py
+++ api/controller.py
@@ -1,29 +1,11 @@
 from aiohttp import web
+from aiohttp_session import get_session
 import jwt
 
 from .auth import permits
-# from aiohttp_security import (remember, forget, authorized_userid,
-#                               check_authorized, check_permission
-#                               )
-from aiohttp_session import get_session
 from .model import *
-
-
-async def test_controller(request):
-    session = await get_session(request)
-    session['hello'] = ''
-    print(session)
-    return web.HTTPOk()
 
 
 async def auth(request):
     permits(request, 'read')
     return web.HTTPOk()
-    # print(request.headers['Accept'])
-    # print(type(request.headers['Accept']))
-    # got_token = request.query['token']
-    # decoded = jwt.decode(got_token, key, algorithms=['HS256'])
-    # # print(decoded)
-    # return web.HTTPUnauthorized()
-
-
