3019f93933b156f53049663b829e1b57b5b199c4
Satyuel <satyuel+git@localhost>
2019-11-10 15:44:34 +0300

    Test check permits

--- api/controller.py
+++ api/controller.py
@@ -1,11 +1,9 @@
 from aiohttp import web
-from aiohttp_session import get_session
-import jwt
 
 from .auth import permits
 from .model import *
 
 
 async def auth(request):
-    permits(request, 'read')
+    permits(request, 0)
     return web.HTTPOk()
